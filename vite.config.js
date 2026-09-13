b05219a766a2a128611d243c888d0c79006526f8
umiphos <umiphos@gmail.com>
2026-09-12 23:37:33 -0600

    [UPDATE]

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-export default defineConfig({ plugins: [react(), tailwindcss()] });
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+});
